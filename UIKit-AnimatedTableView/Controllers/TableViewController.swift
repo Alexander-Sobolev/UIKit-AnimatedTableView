df2a47fcef79739e6e498880eb8bbd99988df1ea
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-23 17:36:11 +0300

    Add Screen

--- UIKit-AnimatedTableView/Controllers/TableViewController.swift
+++ UIKit-AnimatedTableView/Controllers/TableViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateTableView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +30,28 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         cell.textLabel!.text = "Animation"
         return cell
+    }
+    
+    private func animateTableView() {
+        tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableViewHeigh = tableView.bounds.height
+        var delay: Double = 0
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeigh)
+            
+            UIView.animate(withDuration: 1.5,
+                           delay: delay * 0.05,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                            cell.transform = CGAffineTransform.identity
+                           })
+            delay += 1
+        }
+        
     }
 }
